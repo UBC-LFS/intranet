091d20038b1e76e47ce0642fd9b6201506a96b69
exponentian <exponentian.oh@gmail.com>
2023-04-19 16:40:00 -0700

    added 4 hr, fr, op, and comm models

---
+++ finance/models.py
@@ -0,0 +1,51 @@
+from django.conf import settings
+from django.db import models
+
+from wagtail import blocks
+from wagtail.models import Page
+from wagtail.fields import RichTextField, StreamField
+from wagtail.admin.panels import FieldPanel, FieldRowPanel
+from wagtail.search import index
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail.snippets.blocks import SnippetChooserBlock
+
+
+class FinanceIndex(Page):
+    publish_date = models.DateField('Publish date', blank=True, null=True)
+    body = StreamField([
+        ('visual', blocks.RichTextBlock(features=settings.RICH_TEXT_FEATURES)),
+        ('html', blocks.RawHTMLBlock()),
+        ('image', ImageChooserBlock())
+    ], use_json_field=True, blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('body')
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('publish_date'),
+        FieldPanel('body'),
+    ]
+
+    subpage_types = ['FinancePage']
+
+
+class FinancePage(Page):
+    publish_date = models.DateField('Publish date', blank=True, null=True)
+    body = StreamField([
+        ('visual', blocks.RichTextBlock(features=settings.RICH_TEXT_FEATURES)),
+        ('html', blocks.RawHTMLBlock()),
+        ('image', ImageChooserBlock())
+    ], use_json_field=True, blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('body')
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('publish_date'),
+        FieldPanel('body')
+    ]
+
+    parent_page_types = ['FinanceIndex']
+
