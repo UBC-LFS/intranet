e53b37efea836c6bfe0264db9f397c587f51a55c
exponentian <exponentian.oh@gmail.com>
2023-04-26 15:22:50 -0700

    added learning centre model and search function

--- search/views.py
+++ search/views.py
@@ -3,15 +3,47 @@ from django.template.response import TemplateResponse
 
 from wagtail.models import Page
 from wagtail.search.models import Query
+from human_resources import models
+from communications import models
+import re
+
+def html_replace(text):
+    t = re.sub(r'<[^>]*>', r' ', str(text)).split()
+    return ' '.join(t).lower()
+
+def find_page(page, query, results):
+    if page.title.lower().find(query) > -1:
+        results.append(page)
+        return results
+
+    body = None
+    if page.specific.content_type.model == 'communicationpage':
+        body = html_replace(page.communicationpage.body)
+    elif page.specific.content_type.model == 'financepage':
+        body = html_replace(page.financepage.body)
+    elif page.specific.content_type.model == 'humanresourcepage':
+        body = html_replace(page.humanresourcepage.body)
+    elif page.specific.content_type.model == 'operationpage':
+        body = html_replace(page.operationpage.body)            
+
+    if body.find(query) > -1:
+        results.append(page)
+    return results
 
 
 def search(request):
-    search_query = request.GET.get("query", None)
-    page = request.GET.get("page", 1)
+    search_query = request.GET.get('query', '').strip().lower()
+    page = request.GET.get('page', 1)
 
     # Search
     if search_query:
-        search_results = Page.objects.live().search(search_query)
+        search_results = []
+        pages = Page.objects.live()
+        for p in pages:
+            if p.depth == 4:
+                print(p.specific, p.specific.content_type.model)
+                search_results = find_page(p, search_query, search_results)
+                
         query = Query.get(search_query)
 
         # Record hit
@@ -20,7 +52,7 @@ def search(request):
         search_results = Page.objects.none()
 
     # Pagination
-    paginator = Paginator(search_results, 10)
+    paginator = Paginator(search_results, 20)
     try:
         search_results = paginator.page(page)
     except PageNotAnInteger:
@@ -28,11 +60,8 @@ def search(request):
     except EmptyPage:
         search_results = paginator.page(paginator.num_pages)
 
-    return TemplateResponse(
-        request,
-        "search/search.html",
-        {
-            "search_query": search_query,
-            "search_results": search_results,
+    return TemplateResponse(request, 'search/search.html', {
+            'search_query': search_query,
+            'search_results': search_results,
         },
     )
