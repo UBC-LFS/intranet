a791246c7fa97b5994aeb407ac7c62bd4f1deb73
exponentian <exponentian.oh@gmail.com>
2023-11-20 15:24:36 -0800

    fixed a current page

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from wagtail.models import Page
 from django.shortcuts import get_object_or_404
+from django.utils.text import slugify
 
 from core.functions import *
 
@@ -10,7 +12,11 @@ def navbar_menu(request):
 
 
 def current_page(request):
-    page = Page.objects.filter(url_path=request.get_full_path())
+    full_path = request.get_full_path()
+    if full_path == '/':
+        full_path = slugify(settings.WAGTAIL_SITE_NAME)
+
+    page = Page.objects.filter(url_path=full_path)
     return {
         'current_page':  page.first() if page.exists() else None
     }
