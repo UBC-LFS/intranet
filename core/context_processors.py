23b1995f7251a99f2511f75e961d112f3c389d8d
exponentian <exponentian.oh@gmail.com>
2023-11-21 14:25:37 -0800

    added site name slug

--- core/context_processors.py
+++ core/context_processors.py
@@ -5,18 +5,30 @@ from django.utils.text import slugify
 
 from core.functions import *
 
+
 def navbar_menu(request):
     return {
         'navbar_menu': make_menu(request, get_user_groups(request))
     }
 
-
 def current_page(request):
     full_path = request.get_full_path()
     if full_path == '/':
-        full_path = '/' + slugify(settings.WAGTAIL_SITE_NAME) + '/'
+        full_path = make_slug()
 
     page = Page.objects.filter(url_path=full_path)
     return {
         'current_page':  page.first() if page.exists() else None
     }
+
+def site_name_slug(request):
+    return {
+        'site_name_slug': make_slug()
+    }
+
+
+# Helper functions
+
+def make_slug():
+    return '/' + slugify(settings.WAGTAIL_SITE_NAME) + '/'
+    
