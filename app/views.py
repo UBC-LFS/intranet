d97491c6a104aa4e964eb2ae12192e85ca4bb83b
exponentian <exponentian.oh@gmail.com>
2023-11-21 12:06:27 -0800

    added a group for new users

--- app/views.py
+++ app/views.py
@@ -1,12 +1,14 @@
 from django.conf import settings
 from django.shortcuts import render
+from django.contrib.auth import logout
+from django.contrib.auth.models import Group
 from django.http import HttpResponseRedirect
 from django.views import View
 from django.utils.decorators import method_decorator
 from django.views.decorators.http import require_GET
 from django.views.decorators.cache import never_cache
 from django.core.exceptions import SuspiciousOperation
-from django.contrib.auth import logout
+
 
 from core.functions import get_home
 
@@ -30,24 +32,18 @@ class Logout(View):
         return HttpResponseRedirect('/Shibboleth.sso/Logout')
 
 
-def get_data(meta, field):
-    data = settings.SHIB_ATTR_MAP[field]
-    if data in meta:
-        return meta[data]
-    return None
-
-
 @method_decorator([never_cache], name='dispatch')
 class Login(View):
 
     @method_decorator(require_GET)
     def get(self, request, *args, **kwargs):
+
         first_name = get_data(request.META, 'first_name')
         last_name = get_data(request.META, 'last_name')
         email = get_data(request.META, 'email')
         username = get_data(request.META, 'username')
 
-        if not username:
+        if not request.user or not username:
             raise SuspiciousOperation
 
         # Update user information if it's None
@@ -65,6 +61,22 @@ class Login(View):
             update_fields.append('email')
         
         if len(update_fields) > 0:
-            request.user.save(update_fields=update_fields)        
+            request.user.save(update_fields=update_fields)
         
+        # Add a group
+        group, created = Group.objects.get_or_create(name=settings.MEMBERS_ROLE_NAME)
+        if created:
+            print('A new role, Members, has been created successfully')
+
+        request.user.group.add(group)
+
         return HttpResponseRedirect(settings.ADMIN_PORTAL_HOME_PAGE)
+    
+
+# Helper functions
+
+def get_data(meta, field):
+    data = settings.SHIB_ATTR_MAP[field]
+    if data in meta:
+        return meta[data]
+    return None
