913bab66b93da937f97c532c0563de1fec3d1097
exponentian <exponentian.oh@gmail.com>
2025-02-07 11:57:11 -0800

    fixed css

--- core/static/js/core.js
+++ core/static/js/core.js
@@ -20,6 +20,5 @@ $(document).ready(function() {
 
   // Enable Tooltip
   const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-  console.log(tooltipTriggerList);
   const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 });
